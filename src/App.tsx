642e25fb87eb222d8b2f950bcd0c309c7d127dad
Darvin <darvingaba1@gmail.com>
2022-07-27 00:50:24 +0200

    some updates

--- src/App.tsx
+++ src/App.tsx
@@ -18,16 +18,56 @@ function App() {
   // Use initialEmails for state
   // console.log(initialEmails);
   const [emails, setEmails] = useState(initialEmails);
+  const [hiddenEmails, setHiddenEmails] = useState(false);
+  
+  function toggleRead(clickedEmails){
+    const newEmails = structuredClone(emails);
+    const clickedEmail = newEmails.find(email => email.id === clickedEmails.id);
+    clickedEmail.read = !clickedEmail.read;
+    setEmails(newEmails);
+  }
+  function toggleStar(clickedEmails){
+    const newEmails = structuredClone(emails);
+    const clickedEmail = newEmails.find(email => email.id === clickedEmails.id);
+    clickedEmail.starred = !clickedEmail.starred;
+    setEmails(newEmails);
+  }
+  let listOfEmails = emails;
 
-  // function toggleChecked(id: number) {
-  //     emails.map((email:Email) => {
-  //       const newEmail = structuredClone(emails);
-  //       const checkedEmail = newEmail.find((email) => email.id === id);
-  //         checkedEmail.read = !checkedEmail.read;
-  //         setEmails(checkedEmail);
-  //     });
+  if (hiddenEmails) {
+    listOfEmails = emails.filter(email => !email.read);
+  } 
+  function toggleHidden() {
+    setHiddenEmails(!hiddenEmails);
+  }
+  
+
+  let unreadEmails = 0;
+  for (let i = 0; i < listOfEmails.length; i++) {
+    if (!listOfEmails[i].read) {
+      unreadEmails++;
+    }
+  }
+
+  // for (let unreadEmail of listOfEmails) {
+  //   if (!unreadEmail.read) {
+  //     unreadEmail++;
+  //   }
+  // }
+
+  let starredEmails = 0;
+  for (let i = 0; i < listOfEmails.length; i++) {
+    if (listOfEmails[i].starred) {
+      starredEmails++;
+    }
+  }
+  // for (let starred of listOfEmails) {
+  //   if (starred.starred) {
+  //     starredEmails++;
+  //   }
   // }
   return (
+
     <div className="app">
       <Header />
       <nav className="left-menu">
@@ -37,14 +77,16 @@ function App() {
             // onClick={() => {}}
           >
             <span className="label">Inbox</span>
-            <span className="count">?</span>
+            <span className="count">{unreadEmails}</span>
           </li>
           <li
             className="item"
-            // onClick={() => {}}
+            onClick={() => {
+
+            }}
           >
             <span className="label">Starred</span>
-            <span className="count">?</span>
+            <span className="count">{starredEmails}</span>
           </li>
 
           <li className="item toggle">
@@ -53,7 +95,9 @@ function App() {
               id="hide-read"
               type="checkbox"
               checked={false}
-              // onChange={() => {}}
+              onChange={() => {
+                toggleHidden();
+              }}
             />
           </li>
         </ul>
@@ -63,28 +107,27 @@ function App() {
       <main className="emails">
         {/* Render a list of emails here */}
         <ul className="email-list">
-          {initialEmails.map((email:Email) => (
-            <li className= {email.read ? "email read" : "email"}>
-              <div className="read">
+          {listOfEmails.map((email) => (
+            <li className= {email.read ? "email read" : "email unread"}>
                 <input
-                  onChange={() => {
-                  // toggleChecked(email.id);
-                  const newEmail = structuredClone(emails);
-                  console.log(newEmail);
-                  const checkedEmail = newEmail.find((email:Email) => email.id === email.id);
-                  checkedEmail.read = !checkedEmail.read;
-                  setEmails(checkedEmail);
+                  checked={email.read}
+                  onClick={() => {
+                    toggleRead(email);
                   }}
                   type="checkbox"
                 />
-              </div>
 
               <div className="star">
                 <input
+                  
                   className="star-checkbox"
                   type="checkbox"
-                  title="bookmark page"
+                  checked={email.starred}
+                  onClick={() => {
+                    toggleStar(email);
+                  }}
                 />
+
               </div>
 
               <div className="sender">{email.sender}</div>
